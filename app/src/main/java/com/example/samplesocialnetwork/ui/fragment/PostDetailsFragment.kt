c540d3c14b1ccc66aefdf91254588ec3612b08a9
Iman Kianian <iriik@yahoo.com>
2023-02-13 01:54:06 +0330

    Remove unnecessary imports

--- app/src/main/java/com/example/samplesocialnetwork/ui/fragment/PostDetailsFragment.kt
+++ app/src/main/java/com/example/samplesocialnetwork/ui/fragment/PostDetailsFragment.kt
@@ -1,7 +1,6 @@
 package com.example.samplesocialnetwork.ui.fragment
 
 import android.content.Context
-import android.opengl.Visibility
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -10,8 +9,6 @@ import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
-import androidx.core.content.ContextCompat.getSystemService
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
